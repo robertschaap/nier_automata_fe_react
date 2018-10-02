c7fa190940df485c68c6d7a5a50361f09c754d5e
robertschaap <robert.schaap@mac.com>
2018-10-02 23:09:32 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Nier Automata Interface
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div>
+        Nier Automata Interface
+      </div>
+    );
+  }
+}
+
+export default App;
