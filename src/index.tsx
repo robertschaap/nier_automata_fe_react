ca3c9549004aab1524b395bf4c7ee738445e9c12
robertschaap <robert.schaap@mac.com>
2018-10-03 21:00:57 +0200

    styles: added normalize.css

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,10 @@ import * as ReactDOM from 'react-dom';
 import Root from 'containers/Root';
 import registerServiceWorker from 'utils/registerServiceWorker';
 
+// tslint:disable:ordered-imports
+import 'styles/normalize.css';
 import 'styles/index.scss';
+// tslint:enable:ordered-imports
 
 ReactDOM.render(
   <Root />,
