7393c1a9a131488bed55f5666e68589d4ab7c91e
robertschaap <robert.schaap@mac.com>
2018-10-06 23:46:37 +0200

    Navigation: setup base component

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+import styles from './Navigation.scss';
+
+const Navigation = () => {
+  return (
+    <nav className={styles.component}>nav</nav>
+  );
+};
+
+export default Navigation;
