f5134ac6590af59f9e34827a59a7685ad593c9b9
robertschaap <robert.schaap@mac.com>
2018-10-06 23:50:51 +0200

    Main: setup base component

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+import styles from './Main.scss';
+
+const Main = () => {
+  return (
+    <div className={styles.component}>main</div>
+  );
+};
+
+export default Main;
