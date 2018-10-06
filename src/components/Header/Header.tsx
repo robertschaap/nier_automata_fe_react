981425b02064231fe13e4ec83d7be9d177ed7e0a
robertschaap <robert.schaap@mac.com>
2018-10-07 00:16:31 +0200

    Header: setup base component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+import styles from './Header.scss';
+
+const Header = () => {
+  return (
+    <div className={styles.component}>MENU ITEM - sub menu </div>
+  );
+};
+
+export default Header;
