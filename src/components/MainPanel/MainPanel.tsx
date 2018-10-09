27ddb85c1850ef14c59366de9d244df121865e48
robertschaap <robert.schaap@mac.com>
2018-10-09 21:03:53 +0200

    Main: added panel styling and padding

--- src/components/MainPanel/MainPanel.tsx
+++ src/components/MainPanel/MainPanel.tsx
@@ -1,8 +1,15 @@
 import * as React from 'react';
+import styles from './MainPanel.scss';
+
+import Lines from 'components/Lines';
 
 const MainPanel = () => {
   return (
-    <div>MainPanel</div>
+    <div className={styles.component}>
+      <div style={{ marginTop: '51px', width: '58px' }}>
+        <Lines />
+      </div>
+    </div>
   );
 };
 
