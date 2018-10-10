1afaa62f92a57ef6b07cef5e7d9d37ebe7ea5000
robertschaap <robert.schaap@mac.com>
2018-10-10 22:12:14 +0200

    Divider: setup component

---
+++ src/components/Divider/Divider.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import styles from './Divider.scss';
+
+const Divider = () => {
+  return (
+    <hr className={styles.component} />
+  );
+};
+
+export default Divider;
