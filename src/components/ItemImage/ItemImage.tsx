42e0aa14cec76d19f002fea06d62cfd22b0649f2
robertschaap <robert.schaap@mac.com>
2018-10-15 23:25:03 +0200

    ItemImage: setup component

---
+++ src/components/ItemImage/ItemImage.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import styles from './ItemImage.scss';
+
+const ItemImage = () => {
+  return (
+    <div className={styles.component}>
+      Image
+    </div>
+  );
+};
+
+export default ItemImage;
