db4f589fc20015bbb17dedf498d2bed4e4f6a788
robertschaap <robert.schaap@mac.com>
2018-10-23 23:48:00 +0200

    MenuList: added WriteText to items

--- src/components/MenuList/MenuList.tsx
+++ src/components/MenuList/MenuList.tsx
@@ -3,6 +3,7 @@ import styles from './MenuList.scss';
 
 import Lines from 'components/Lines';
 import NavItem from 'components/NavItem';
+import WriteText from 'components/WriteText';
 
 const MenuList = () => {
   return (
@@ -12,13 +13,13 @@ const MenuList = () => {
           <Lines />
         </div>
         <ul className={styles.list}>
-          <NavItem label='All Items' to='#' list={true} />
-          <NavItem label='Restorative Items' to='#' list={true} />
-          <NavItem label='Enhancement Items' to='#' list={true} />
-          <NavItem label='Support Items' to='#' list={true} />
-          <NavItem label='Materials' to='#' list={true} />
-          <NavItem label='Key Items' to='#' list={true} />
-          <NavItem label='Caught Items' to='#' list={true} />
+          <NavItem label={<WriteText label='All Items' />} to='#' list={true} />
+          <NavItem label={<WriteText label='Restorative Items' />} to='#' list={true} />
+          <NavItem label={<WriteText label='Enhancement Items' />} to='#' list={true} />
+          <NavItem label={<WriteText label='Support Items' />} to='#' list={true} />
+          <NavItem label={<WriteText label='Materials' />} to='#' list={true} />
+          <NavItem label={<WriteText label='Key Items' />} to='#' list={true} />
+          <NavItem label={<WriteText label='Caught Items' />} to='#' list={true} />
         </ul>
       </div>
     </div>
