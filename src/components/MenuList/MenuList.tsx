c6f7ff5b9b20e691515e20f706fc8c6e5c0898fb
robertschaap <robert.schaap@mac.com>
2018-10-23 23:11:36 +0200

    MenuList: basic setup of component

---
+++ src/components/MenuList/MenuList.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import styles from './MenuList.scss';
+
+import Lines from 'components/Lines';
+import NavItem from 'components/NavItem';
+
+const MenuList = () => {
+  return (
+    <div className={styles.component}>
+      <div className={styles.container}>
+        <div style={{ width: '58px' }}>
+          <Lines />
+        </div>
+        <ul className={styles.list}>
+          <NavItem label='Restorative Items' to='#' list={true} />
+          <NavItem label='Enhancement Items' to='#' list={true} />
+          <NavItem label='Support Items' to='#' list={true} />
+          <NavItem label='Materials' to='#' list={true} />
+          <NavItem label='Key Items' to='#' list={true} />
+          <NavItem label='Caught Items' to='#' list={true} />
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default MenuList;
