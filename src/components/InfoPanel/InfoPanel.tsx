e80de29e156c4ccb3770e218c4da4429cf5934da
robertschaap <robert.schaap@mac.com>
2018-10-24 23:33:02 +0200

    InfoPanel: changed placeholder

--- src/components/InfoPanel/InfoPanel.tsx
+++ src/components/InfoPanel/InfoPanel.tsx
@@ -15,7 +15,7 @@ const InfoPanel = (props: InfoPanelProps) => {
         <div className={styles.label}>
           {props.label}
         </div>
-        <div className={styles.buttons}>buttons</div>
+        <div className={styles.buttons}>Select Confirm</div>
       </div>
     </footer>
   );
