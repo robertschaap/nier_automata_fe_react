29d2a2ec8cda4a5b0430439a694c3e7fa05e4501
robertschaap <robert.schaap@mac.com>
2018-10-31 23:36:41 +0100

    ItemList: base setup of component

---
+++ src/components/ItemList/ItemList.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import styles from './ItemList.scss';
+
+import Lines from 'components/Lines';
+import NavItem from 'components/NavItem';
+
+// TODO: this is very similar to <MenuList />, refactor this once the APIs are clearer
+const ItemList = () => {
+  return (
+    <div className={styles.component}>
+      <div className={styles.container}>
+        <div style={{ width: '58px' }}>
+          <Lines />
+        </div>
+        <ul className={styles.list}>
+          <NavItem label='Small Recovery' to='#' list={true} />
+          <NavItem label='Medium Recovery' to='#' list={true} />
+          <NavItem label='Large Recovery' to='#' list={true} />
+          <NavItem label='Full Recovery' to='#' list={true} />
+          <NavItem label='Visual Cure' to='#' list={true} />
+          <NavItem label='Aural Cure' to='#' list={true} />
+          <NavItem label='Cure Manipulation' to='#' list={true} />
+          <NavItem label='Cure All Status' to='#' list={true} />
+          <NavItem label='Cure All + Heal All' to='#' list={true} />
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default ItemList;
