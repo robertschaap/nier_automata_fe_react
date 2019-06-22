7697e7a41e41ee29ac8145474bff6e64a9e19d24
robertschaap <robert.schaap@mac.com>
2019-06-22 16:32:27 +0200

    AilmentsPanel: fix missing key error

--- src/components/AilmentsPanel/AilmentsPanel.tsx
+++ src/components/AilmentsPanel/AilmentsPanel.tsx
@@ -2,9 +2,9 @@ import * as React from 'react';
 import styles from './AilmentsPanel.scss';
 
 const AilmentsPanel = () => {
-  const ailments = Array(11).fill(
-    <div className={styles.ailment} />,
-  );
+  const ailments = Array(11).fill(0).map((el, index) => (
+    <div key={index} className={styles.ailment} />
+  ));
 
   return (
     <div className={styles.component}>
