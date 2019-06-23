b9ec574985ca0f6e8e2c4c14244e2c8d1389cfe6
robertschaap <robert.schaap@mac.com>
2019-06-23 20:03:04 +0200

    Navigation: added routes

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import styles from './Navigation.scss';
 
+import Routes from 'constants/Routes';
+
 import Lines from 'components/Lines';
 import NavItem from 'components/NavItem';
 
@@ -10,13 +12,13 @@ const Navigation = () => {
       <div style={{ marginTop: '6px', height: '51px', width: '58px' }}>
         <Lines />
       </div>
-      <NavItem isActive={true} label='Map' to='#' />
-      <NavItem label='Quests' to='#' />
-      <NavItem label='Items' to='#' />
-      <NavItem label='Weapons' to='#' />
-      <NavItem label='Skills' to='#' />
-      <NavItem label='Intel' to='#' />
-      <NavItem label='System' to='#' />
+      <NavItem isActive={true} label='Map' to={Routes.MAP} />
+      <NavItem label='Quests' to={Routes.QUESTS} />
+      <NavItem label='Items' to={Routes.ITEMS} />
+      <NavItem label='Weapons' to={Routes.WEAPONS} />
+      <NavItem label='Skills' to={Routes.SKILLS} />
+      <NavItem label='Intel' to={Routes.INTEL} />
+      <NavItem label='System' to={Routes.SYSTEM} />
     </nav>
   );
 };
