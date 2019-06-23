802a8deed3df22218ffde3114fa7982b6aa49743
robertschaap <robert.schaap@mac.com>
2019-06-23 19:09:46 +0200

    Routes: setup constants file

---
+++ src/constants/Routes.ts
@@ -0,0 +1,12 @@
+const Routes = {
+  INDEX: '/',
+  INTEL: '/intel',
+  ITEMS: '/items',
+  MAP: '/map',
+  QUESTS: '/quests',
+  SKILLS: '/skills',
+  SYSTEM: '/system',
+  WEAPONS: '/weapons',
+};
+
+export default Routes;
