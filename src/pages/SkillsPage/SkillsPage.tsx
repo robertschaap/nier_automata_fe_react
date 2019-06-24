14025d656a858eb4d7e2c3d6ee5497e714966fd3
robertschaap <robert.schaap@mac.com>
2019-06-24 21:05:26 +0200

    SkillsPage: setup component

---
+++ src/pages/SkillsPage/SkillsPage.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+
+import Header from 'components/Header';
+import InfoPanel from 'components/InfoPanel';
+import Main from 'components/Main';
+import MainPanel from 'components/MainPanel';
+import MenuList from 'components/MenuList';
+import StatusPanel from 'components/StatusPanel';
+
+const SkillsPage = () => {
+  return (
+    <>
+      <Main>
+        <Header label='Skills' />
+        <MainPanel>
+          <MenuList />
+          <StatusPanel />
+        </MainPanel>
+      </Main>
+      <InfoPanel label='Change equipped weapons' />
+    </>
+  );
+};
+
+export default SkillsPage;
