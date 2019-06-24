fb0e91daccd693e16c2c6715073ea25291f34546
robertschaap <robert.schaap@mac.com>
2019-06-24 21:04:32 +0200

    App: add IntelPage to router

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Layout from 'components/Layout';
 import ViewportContainer from 'containers/ViewportContainer';
 
+import IntelPage from 'pages/IntelPage';
 import ItemsPage from 'pages/ItemsPage';
 import MapPage from 'pages/MapPage';
 import QuestsPage from 'pages/QuestsPage';
@@ -22,6 +23,7 @@ class App extends React.Component {
             <Route path={Routes.MAP} component={MapPage} />
             <Route path={Routes.ITEMS} component={ItemsPage} />
             <Route path={Routes.WEAPONS} component={WeaponsPage} />
+            <Route path={Routes.INTEL} component={IntelPage} />
           </Layout>
         </ViewportContainer>
       </BrowserRouter>
