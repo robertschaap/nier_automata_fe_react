e294ad11633fad2beb3886f2bbe2b1e070f0afd6
robertschaap <robert.schaap@mac.com>
2019-07-06 15:07:08 +0200

    SystemPage: add menu options

--- src/pages/SystemPage/SystemPage.tsx
+++ src/pages/SystemPage/SystemPage.tsx
@@ -7,13 +7,24 @@ import MainPanel from 'components/MainPanel';
 import MenuList from 'components/MenuList';
 import StatusPanel from 'components/StatusPanel';
 
+const options = [
+  { label: 'Save' },
+  { label: 'Load' },
+  { label: 'Settings' },
+  { label: 'Controls' },
+  { label: 'Network' },
+  { label: 'Play Records' },
+  { label: 'Return to Title Screen' },
+  { label: 'Exit' },
+];
+
 const SystemPage = () => {
   return (
     <>
       <Main>
         <Header label='System' />
         <MainPanel>
-          <MenuList />
+          <MenuList options={options} />
           <StatusPanel />
         </MainPanel>
       </Main>
