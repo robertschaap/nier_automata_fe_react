ed465847aafc417f6352e812cffcc9256276e83d
robertschaap <robert.schaap@mac.com>
2019-07-06 15:08:47 +0200

    WeaponsPage: add menu options and remove item panel

--- src/pages/WeaponsPage/WeaponsPage.tsx
+++ src/pages/WeaponsPage/WeaponsPage.tsx
@@ -2,20 +2,24 @@ import * as React from 'react';
 
 import Header from 'components/Header';
 import InfoPanel from 'components/InfoPanel';
-import ItemPanel from 'components/ItemPanel';
 import Main from 'components/Main';
 import MainPanel from 'components/MainPanel';
 import MenuList from 'components/MenuList';
 import StatusPanel from 'components/StatusPanel';
 
+const options = [
+  { label: 'Weapons' },
+  { label: 'Weapons Set 1' },
+  { label: 'Weapons Set 2' },
+];
+
 const WeaponsPage = () => {
   return (
     <>
       <Main>
-        <Header label='Weapons' info='Weapon Set 1' />
+        <Header label='Weapons' />
         <MainPanel>
-          <MenuList />
-          <ItemPanel />
+          <MenuList options={options} />
           <StatusPanel />
         </MainPanel>
       </Main>
