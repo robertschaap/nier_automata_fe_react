f5773d357866cc620dec1e4564fe50126023e634
robertschaap <robert.schaap@mac.com>
2019-07-06 15:36:00 +0200

    MenuItem: render regular item or navlink

--- src/components/MenuItem/MenuItem.tsx
+++ src/components/MenuItem/MenuItem.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import cx from 'classnames';
 import styles from './MenuItem.scss';
@@ -7,22 +8,42 @@ export interface MenuItemProps {
   icon?: string; // TODO wire
   isActive?: boolean;
   label: string | React.ReactNode;
-  to?: string; // TODO wire
+  to?: string;
 }
 
 const MenuItem = (props: MenuItemProps) => {
-  const { isActive, label } = props;
+  const {
+    isActive,
+    label,
+    to,
+  } = props;
+
+  const menuItemBody = (
+    <>
+      <div className={styles.fill} />
+      <div className={styles.label}>
+        <i className={styles.icon} />
+        {label}
+      </div>
+    </>
+  );
+
+  if (to) {
+    return (
+      <div className={styles.component}>
+        <NavLink className={styles.bar} activeClassName={styles.isActive} to={to}>
+          {menuItemBody}
+        </NavLink>
+      </div>
+    );
+  }
 
   return (
     <div className={styles.component}>
       <div className={cx(styles.bar, {
         [styles.active]: isActive,
       })}>
-        <div className={styles.fill} />
-        <div className={styles.label}>
-          <i className={styles.icon} />
-          {label}
-        </div>
+        {menuItemBody}
       </div>
     </div>
   );
