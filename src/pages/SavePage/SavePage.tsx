4808c8acbedb032e14dc99f00c7881e759c6c4dc
robertschaap <robert.schaap@mac.com>
2019-07-07 14:09:25 +0200

    SavePage: setup basic page

---
+++ src/pages/SavePage/SavePage.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+import Header from 'components/Header';
+import InfoPanel from 'components/InfoPanel';
+import Layout from 'components/Layout';
+import Main from 'components/Main';
+
+const SavePage = () => {
+  return (
+    <Layout showNavigation={false}>
+      <Main>
+        <Header label='Save game' />
+      </Main>
+      <InfoPanel label='Select a file to save to' />
+    </Layout>
+  );
+};
+
+export default SavePage;
