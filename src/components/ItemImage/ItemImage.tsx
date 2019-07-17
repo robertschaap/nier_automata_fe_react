f93395f5bc301d07c008ac9a5b6d4fdefaefb6f5
robertschaap <robert.schaap@mac.com>
2019-07-17 19:47:40 +0200

    ItemImage: add component type and comment

--- src/components/ItemImage/ItemImage.tsx
+++ src/components/ItemImage/ItemImage.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import styles from './ItemImage.scss';
 
-const ItemImage = () => {
+// TODO: wire image behaviour
+const ItemImage: React.FunctionComponent = () => {
   return (
     <div className={styles.component}>
       <div>Image</div>
