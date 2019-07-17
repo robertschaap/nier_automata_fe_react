54fa884dd888715afb6bcf8c5c7a4c333bd8a5c1
robertschaap <robert.schaap@mac.com>
2019-07-17 19:41:45 +0200

    index: clarify tslint overwrite

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import * as ReactDOM from 'react-dom';
 import Root from 'containers/Root';
 import registerServiceWorker from 'utils/registerServiceWorker';
 
+// Disabled because first want to normalize browser styles before loading our own
 // tslint:disable:ordered-imports
 import 'styles/normalize.css';
 import 'styles/index.scss';
