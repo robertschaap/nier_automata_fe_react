3c147173000872327ffec34267ecbb78403ca966
robertschaap <robert.schaap@mac.com>
2020-10-26 21:10:47 +0100

    chore: boostrap snowpack application

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,7 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDom from 'react-dom';
 
-import Root from 'containers/Root';
-import registerServiceWorker from 'utils/registerServiceWorker';
-
-// Disabled because first want to normalize browser styles before loading our own
-// tslint:disable:ordered-imports
-import 'styles/normalize.css';
-import 'styles/index.scss';
-// tslint:enable:ordered-imports
-
-ReactDOM.render(
-  <Root />,
-  document.getElementById('root') as HTMLElement,
+ReactDom.render(
+  <div>Nier Automata</div>,
+  document.getElementById('root'),
 );
-registerServiceWorker();
