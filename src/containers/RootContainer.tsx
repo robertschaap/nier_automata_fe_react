5baa6cadd8ba512acadb8d73805ff333bd9bcb68
robertschaap <robert.schaap@mac.com>
2020-11-10 21:34:25 +0100

    feat(RootContainer): setup component

---
+++ src/containers/RootContainer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { ThemeProviderContainer } from './ThemeProviderContainer';
+
+export const RootContainer: React.FC = () => {
+  return (
+    <ThemeProviderContainer>
+      <div>Nier Automata</div>
+    </ThemeProviderContainer>
+  );
+};
