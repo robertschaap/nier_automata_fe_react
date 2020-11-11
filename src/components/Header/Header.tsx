ae3e7f379a3a3ac88eeeb931f34c8323f182daa2
robertschaap <robert.schaap@mac.com>
2020-11-11 16:39:40 +0100

    feat(Header): setup base component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const Header: React.FC = () => {
+  return (
+    <header>
+      <nav>
+        <span>Map</span>
+        <span>Quests</span>
+        <span>Items</span>
+        <span>Weapons</span>
+        <span>Skills</span>
+        <span>Intel</span>
+        <span>System</span>
+      </nav>
+    </header>
+  );
+};
