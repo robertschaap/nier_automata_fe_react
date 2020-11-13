36adf265ce12fe815d7ac0e2e5b08694df809d5f
robertschaap <robert.schaap@mac.com>
2020-11-13 17:12:51 +0100

    feat(Footer): allow title and preliminary styling

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
+import * as S from './footer.styles';
+
 import { HorizontalRule } from 'components/HorizontalRule';
 
-export const Footer: React.FC = () => {
+interface FooterProps {
+  text?: string;
+}
+
+export const Footer: React.FC<FooterProps> = ({ text }) => {
   return (
     <footer>
-      <div>footer</div>
+      <S.InfoPanel>{text ?? 'footer'}</S.InfoPanel>
       <HorizontalRule />
     </footer>
   );
---
+++ src/components/Footer/footer.styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const InfoPanel = styled.div`
+  margin: 0 50px;
+  background-color: ${({ theme }) => theme.color.lightest};
+`;
