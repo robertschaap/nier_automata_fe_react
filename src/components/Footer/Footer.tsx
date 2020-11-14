023139be2e420e35ce6c310b44ef1181c53b0a5b
robertschaap <robert.schaap@mac.com>
2020-11-14 10:49:49 +0100

    feat(Footer): add base styling

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,13 +5,21 @@ import * as S from './footer.styles';
 import { HorizontalRule } from 'components/HorizontalRule';
 
 interface FooterProps {
-  text?: string;
+  text: string;
 }
 
 export const Footer: React.FC<FooterProps> = ({ text }) => {
   return (
     <footer>
-      <S.InfoPanel>{text ?? 'footer'}</S.InfoPanel>
+      <S.InfoPanel>
+        <S.Decoration />
+        <S.Label>
+          {text}
+        </S.Label>
+        <S.Buttons>
+          Select Contirm Back
+        </S.Buttons>
+      </S.InfoPanel>
       <HorizontalRule />
     </footer>
   );
--- src/components/Footer/footer.styles.ts
+++ src/components/Footer/footer.styles.ts
@@ -1,6 +1,25 @@
 import styled from 'styled-components';
 
 export const InfoPanel = styled.div`
+  display: flex;
+  align-items: center;
   margin: 0 50px;
+  height: 80px;
   background-color: ${({ theme }) => theme.color.lightest};
+  box-shadow: 3px 3px ${({ theme }) => theme.color.normal};
+`;
+
+export const Label = styled.div`
+  flex: auto;
+`;
+
+export const Decoration = styled.div`
+  background-color: hotpink;
+  width: 25px;
+  height: 100%;
+  margin-right: 35px;
+`;
+
+export const Buttons = styled.div`
+  padding-right: 35px;
 `;
