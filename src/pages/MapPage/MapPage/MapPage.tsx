63c345f2f111ee0035baea5eefdcce3cd7ec8f0f
robertschaap <robert.schaap@mac.com>
2020-11-19 21:11:21 +0100

    feat(MapPageContainer): setup crude layout

--- src/pages/MapPage/MapPage/MapPage.tsx
+++ src/pages/MapPage/MapPage/MapPage.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { Map } from '../Map';
+
 const Main = styled.main`
   display: flex;
+  flex: auto;
+  margin: 50px;
+  margin-top: 35px;
 
-  div:first-child {
+  > div:first-child {
     flex: 1 1 0;
-    border: 1px solid hotpink;
     background-color: #eee;
   }
 
-  div {
+  > div {
     flex: 2 2 0;
-    border: 1px solid hotpink;
     background-color: #eee;
   }
 `;
@@ -20,8 +23,21 @@ const Main = styled.main`
 export const MapPage: React.FC = () => {
   return (
     <Main>
-      <div>Items</div>
-      <div>Map</div>
+      <div>
+        <ul>
+          <li>Quick Save</li>
+          <li>Map Mode</li>
+          <li>Map Icons</li>
+        </ul>
+        <div>
+          <div>City Ruins</div>
+          <div>MIA</div>
+          <div>Use your scanner to find surviving YoRHa</div>
+        </div>
+      </div>
+      <div>
+        <Map />
+      </div>
     </Main>
   );
 };
--- src/pages/MapPage/MapPageContainer.tsx
+++ src/pages/MapPage/MapPageContainer.tsx
@@ -5,12 +5,14 @@ import { MapPage } from './MapPage';
 import { Footer } from 'components/Footer';
 import { Header } from 'components/Header';
 
+// TODO: the inline styles are temp, remove when there's a bit of clarity on how
+// the Main component inside(?) MapPage acts and on what Footer needs
 export const MapPageContainer: React.FC = () => {
   return (
-    <>
+    <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
       <Header title="Map" />
       <MapPage />
-      <Footer />
-    </>
+      <Footer text="View the map or perform a quick save" />
+    </div>
   );
 };
