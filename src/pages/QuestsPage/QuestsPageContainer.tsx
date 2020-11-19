fd80e0b01681972e8503185a2d64d54affc45388
robertschaap <robert.schaap@mac.com>
2020-11-19 21:11:09 +0100

    feat(QuestsPageContainer): setup crude layout

--- src/pages/QuestsPage/QuestsPageContainer.tsx
+++ src/pages/QuestsPage/QuestsPageContainer.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import { Footer } from 'components/Footer';
+import { Header } from 'components/Header';
+
 export const QuestsPageContainer: React.FC = () => {
   return (
-    <div>Page</div>
+    <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+      <Header title="Quests" />
+      <div>Page</div>
+      <Footer text="View the map or perform a quick save" />
+    </div>
   );
 };
