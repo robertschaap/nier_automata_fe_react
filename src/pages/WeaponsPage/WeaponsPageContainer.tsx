3050d0c88c8ce5f2bb9459833be8c532be4a3ccd
robertschaap <robert.schaap@mac.com>
2020-11-20 21:56:24 +0100

    feat(WeaponsPageContainer): add crude layout

--- src/pages/WeaponsPage/WeaponsPageContainer.tsx
+++ src/pages/WeaponsPage/WeaponsPageContainer.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import { Footer } from 'components/Footer';
+import { Header } from 'components/Header';
+
 export const WeaponsPageContainer: React.FC = () => {
   return (
-    <div>Page</div>
+    <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+      <Header title="Weapons" />
+      <div>WeaponsPage</div>
+      <Footer text="Change equipped weapons" />
+    </div>
   );
 };
