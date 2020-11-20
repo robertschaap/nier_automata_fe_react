a77b9debecf81589c9ad17905be7e1741dad9bbe
robertschaap <robert.schaap@mac.com>
2020-11-20 21:56:40 +0100

    feat(SkillsPageContainer): add crude layout

--- src/pages/SkillsPage/SkillsPageContainer.tsx
+++ src/pages/SkillsPage/SkillsPageContainer.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import { Footer } from 'components/Footer';
+import { Header } from 'components/Header';
+
 export const SkillsPageContainer: React.FC = () => {
   return (
-    <div>Page</div>
+    <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+      <Header title="Skills" />
+      <div>SkillsPage</div>
+      <Footer text="Assign special abilities." />
+    </div>
   );
 };
