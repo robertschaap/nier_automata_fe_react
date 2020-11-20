3d5f6fe4aac0a87dc69e6fd27ac67b75e15ced33
robertschaap <robert.schaap@mac.com>
2020-11-20 21:59:29 +0100

    feat(IntelPageContainer): setup crude layout

--- src/pages/IntelPage/IntelPageContainer.tsx
+++ src/pages/IntelPage/IntelPageContainer.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import { Footer } from 'components/Footer';
+import { Header } from 'components/Header';
+
 export const IntelPageContainer: React.FC = () => {
   return (
-    <div>Page</div>
+    <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+      <Header title="Intel" />
+      <div>ItemsPage</div>
+      <Footer text="View various types of aquired data." />
+    </div>
   );
 };
