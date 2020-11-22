bc817779600ec4692b23750da67bfa3cf87ecc1e
robertschaap <robert.schaap@mac.com>
2020-11-22 15:16:44 +0100

    feat(ItemsPageContainer): add crude layout

--- src/pages/ItemsPage/ItemsPageContainer.tsx
+++ src/pages/ItemsPage/ItemsPageContainer.tsx
@@ -2,12 +2,28 @@ import React from 'react';
 
 import { Footer } from 'components/Footer';
 import { Header } from 'components/Header';
+import { Main } from 'components/Main';
+import { MenuList } from 'components/MenuList';
+import { StatusPanel } from 'components/StatusPanel';
+
+const items = [
+  { label: 'All Items' },
+  { label: 'Restorative Items' },
+  { label: 'Enhancement Items' },
+  { label: 'Support Items' },
+  { label: 'Materials' },
+  { label: 'Key Items' },
+  { label: 'Caught Fish' },
+];
 
 export const ItemsPageContainer: React.FC = () => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
       <Header title="Items" />
-      <div>ItemsPage</div>
+      <Main spacing="space-between">
+        <MenuList items={items} />
+        <StatusPanel />
+      </Main>
       <Footer text="View or use items in your inventory." />
     </div>
   );
