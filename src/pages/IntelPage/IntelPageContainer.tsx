8c7929b408d76c7ed2e171a10c7337d96ecb4463
robertschaap <robert.schaap@mac.com>
2020-11-22 15:16:07 +0100

    feat(IntelPageContainer): add crude layout

--- src/pages/IntelPage/IntelPageContainer.tsx
+++ src/pages/IntelPage/IntelPageContainer.tsx
@@ -2,12 +2,28 @@ import React from 'react';
 
 import { Footer } from 'components/Footer';
 import { Header } from 'components/Header';
+import { Main } from 'components/Main';
+import { MenuList } from 'components/MenuList';
+import { StatusPanel } from 'components/StatusPanel';
+
+const items = [
+  { label: 'Archives', info: '73%' },
+  { label: 'Unit Data', info: '89%' },
+  { label: 'Tutorials', info: '98%' },
+  { label: 'Weapons Stories', info: '47%' },
+  { label: 'Picture Books', info: '87%' },
+  { label: 'Fishing Encyclopedia', info: '34%' },
+  { label: 'Novel', info: '100%' },
+];
 
 export const IntelPageContainer: React.FC = () => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
       <Header title="Intel" />
-      <div>ItemsPage</div>
+      <Main spacing="space-between">
+        <MenuList items={items} />
+        <StatusPanel />
+      </Main>
       <Footer text="View various types of aquired data." />
     </div>
   );
