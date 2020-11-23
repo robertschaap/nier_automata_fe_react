9a5dcd6e1c083255a5f6f9d58f8aab13043f5fd6
robertschaap <robert.schaap@mac.com>
2020-11-23 19:46:08 +0100

    feat(SkillsPageContainer): add crude layout

--- src/pages/SkillsPage/SkillsPageContainer.tsx
+++ src/pages/SkillsPage/SkillsPageContainer.tsx
@@ -2,12 +2,23 @@ import React from 'react';
 
 import { Footer } from 'components/Footer';
 import { Header } from 'components/Header';
+import { Main } from 'components/Main';
+import { MenuList } from 'components/MenuList';
+import { StatusPanel } from 'components/StatusPanel';
+
+const items = [
+  { label: 'Pod Programs', info: '88%' },
+  { label: 'Plug-in Chips' },
+];
 
 export const SkillsPageContainer: React.FC = () => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
       <Header title="Skills" />
-      <div>SkillsPage</div>
+      <Main spacing="space-between">
+        <MenuList items={items} />
+        <StatusPanel />
+      </Main>
       <Footer text="Assign special abilities." />
     </div>
   );
