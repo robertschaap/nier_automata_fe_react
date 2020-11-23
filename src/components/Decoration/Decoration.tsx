24144b6262dd61d9d6468e03a583efdb40e96168
robertschaap <robert.schaap@mac.com>
2020-11-23 20:57:10 +0100

    fix(Decoration): fix issues with min height of 150

--- src/components/Decoration/Decoration.tsx
+++ src/components/Decoration/Decoration.tsx
@@ -8,7 +8,7 @@ interface DecorationProps {
 
 export const Decoration: React.FC<DecorationProps> = ({ isDark }) => {
   return (
-    <S.DecorationBase isDark={isDark} width='25px' height='100%'>
+    <S.DecorationBase preserveAspectRatio="none" viewBox="0 0 25 100" isDark={isDark} width='25px' height='100%'>
       <S.Rectangle width='14' height='100%' />
       <S.Rectangle width='3' height='100%' transform='translate(22)' />
     </S.DecorationBase>
