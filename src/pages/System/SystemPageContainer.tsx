0bd95acf2055ea9ceb57b53072f41541178223a0
robertschaap <robert.schaap@mac.com>
2020-11-23 19:46:23 +0100

    feat(SystemPageContainer): add crude layout

--- src/pages/System/SystemPageContainer.tsx
+++ src/pages/System/SystemPageContainer.tsx
@@ -2,12 +2,28 @@ import React from 'react';
 
 import { Footer } from 'components/Footer';
 import { Header } from 'components/Header';
+import { Main } from 'components/Main';
+import { MenuList } from 'components/MenuList';
+import { StatusPanel } from 'components/StatusPanel';
+
+const items = [
+  { label: 'Save', isDisabled: true },
+  { label: 'Load', isDisabled: true },
+  { label: 'Settings' },
+  { label: 'Controls' },
+  { label: 'Network' },
+  { label: 'Play Records' },
+  { label: 'Return to Title Screen' },
+];
 
 export const SystemPageContainer: React.FC = () => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
       <Header title="System" showNavigation={false} />
-      <div>SystemPage</div>
+      <Main spacing="space-between">
+        <MenuList items={items} />
+        <StatusPanel />
+      </Main>
       <Footer text="Load game, save game, or change system settings." />
     </div>
   );
