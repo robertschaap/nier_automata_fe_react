3454be0c740de64f76d5c66b248e2eb722e3cab8
robertschaap <robert.schaap@mac.com>
2020-11-23 19:46:37 +0100

    feat(WeaponsPageContainer): add crude layout

--- src/pages/WeaponsPage/WeaponsPageContainer.tsx
+++ src/pages/WeaponsPage/WeaponsPageContainer.tsx
@@ -2,12 +2,24 @@ import React from 'react';
 
 import { Footer } from 'components/Footer';
 import { Header } from 'components/Header';
+import { Main } from 'components/Main';
+import { MenuList } from 'components/MenuList';
+import { StatusPanel } from 'components/StatusPanel';
+
+const items = [
+  { label: 'Weapons', info: '87%' },
+  { label: 'Weapon Set 1' },
+  { label: 'Weapon Set 2' },
+];
 
 export const WeaponsPageContainer: React.FC = () => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
       <Header title="Weapons" />
-      <div>WeaponsPage</div>
+      <Main spacing="space-between">
+        <MenuList items={items} />
+        <StatusPanel />
+      </Main>
       <Footer text="Change equipped weapons" />
     </div>
   );
