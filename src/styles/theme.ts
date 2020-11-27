3b238e07021c4048ba14fe973b1418ada0618ed5
robertschaap <robert.schaap@mac.com>
2020-11-27 19:32:13 +0100

    feat(theme): add hex to rgba formatting utility

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -11,26 +11,37 @@ const color = {
   darkest: '#4e4c44',
 };
 
+/**
+ * Converts a hexadecimal code to the `rgba(r,g,b,a)` format.
+ * @param hexCode Hexadecimal code in either upper or lower case with hash prefix
+ * @param alpha Number between 0 and 1
+ */
+const toRgba = (hexCode: string, alpha: number) => {
+  if (hexCode.length !== 7) {
+    return 'fout';
+  }
+
+  const rgb = hexCode.split('').map((c) => '0123456789abcdef'.indexOf(c.toLowerCase()));
+  rgb.shift();
+
+  if (rgb.some((e) => e === -1) || alpha < 0 || alpha > 1) {
+    return null;
+  }
+
+  const r = (rgb[0] * 16) + rgb[1];
+  const g = (rgb[2] * 16) + rgb[3];
+  const b = (rgb[4] * 16) + rgb[5];
+
+  return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+};
+
+const utils = {
+  toRgba,
+};
+
+// section widths are either 535, 572, or
 export const theme = Object.freeze({
   aspectRatio,
   color,
+  utils,
 });
-
-/**
- * Base styles for reference
- *
- * $text-color: $color-key-darkest;
- * $text-color-light: $color-key-lightest;
- *
- * $bg-main-color: $color-key-light;
- * $bg-light-color: $color-key-lightest;
- *
- * $navitem-color: $color-key-normal;
- * $navitem-focus-color: $color-key-darkest;
- *
- * $panel-header-bg-color: $color-key-darkest;
- *
- * $decoration-color: $color-key-darkest;
- * $lines-color: $color-key-normal;
- * $lines-color-dark: $color-key-darkest;
- */
