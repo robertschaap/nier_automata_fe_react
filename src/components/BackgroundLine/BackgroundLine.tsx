12d825171d62f88063772802b38cde6b94023a78
robertschaap <robert.schaap@mac.com>
2020-11-28 13:35:43 +0100

    fix(BackgroundLine): fix incorrect props

--- src/components/BackgroundLine/BackgroundLine.tsx
+++ src/components/BackgroundLine/BackgroundLine.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export const BackgroundLine: React.FC = () => {
   return (
-    <svg width="1067" height="1067" xmlns="http://www.w3.org/2000/svg">
-      <path stroke="#000" stroke-width="3" d="M2.5 2.5l1062 1062" fill="none" fill-rule="evenodd" stroke-linecap="square"/>
+    <svg width="1067" height="1067">
+      <path stroke="#000" strokeWidth="3" d="M2.5 2.5l1062 1062" fill="none" fillRule="evenodd" strokeLinecap="square"/>
     </svg>
   );
 };
