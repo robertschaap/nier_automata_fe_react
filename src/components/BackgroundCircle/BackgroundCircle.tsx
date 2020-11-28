ade9f47b1e85e0e91b5e70c9934115dbf5325bd2
robertschaap <robert.schaap@mac.com>
2020-11-28 10:56:31 +0100

    feat(BackgroundCircle): setup component

---
+++ src/components/BackgroundCircle/BackgroundCircle.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const BackgroundCircle: React.FC = () => {
+  return (
+    <svg width="1181" height="1181" xmlns="http://www.w3.org/2000/svg">
+      <circle stroke="#000" stroke-width="3" cx="590.5" cy="590.5" r="588.5" fill="none" fill-rule="evenodd"/>
+    </svg>
+  );
+};
---
+++ src/components/BackgroundCircle/index.ts
@@ -0,0 +1,1 @@
+export { BackgroundCircle } from './BackgroundCircle';
