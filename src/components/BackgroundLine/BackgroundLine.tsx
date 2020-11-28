fdd51d65b6fad37d7f912d6f1fa098e9d7f91b32
robertschaap <robert.schaap@mac.com>
2020-11-28 10:56:42 +0100

    feat(BackgroundLine): setup component

---
+++ src/components/BackgroundLine/BackgroundLine.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const BackgroundLine: React.FC = () => {
+  return (
+    <svg width="1067" height="1067" xmlns="http://www.w3.org/2000/svg">
+      <path stroke="#000" stroke-width="3" d="M2.5 2.5l1062 1062" fill="none" fill-rule="evenodd" stroke-linecap="square"/>
+    </svg>
+  );
+};
---
+++ src/components/BackgroundLine/index.ts
@@ -0,0 +1,1 @@
+export { BackgroundLine } from './BackgroundLine';
