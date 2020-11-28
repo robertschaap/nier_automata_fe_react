b998b5ae87078bbf8f1eb56e4d2678740e7ec38b
robertschaap <robert.schaap@mac.com>
2020-11-28 13:35:35 +0100

    fix(BackgroundCircle): fix incorrect props

--- src/components/BackgroundCircle/BackgroundCircle.tsx
+++ src/components/BackgroundCircle/BackgroundCircle.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 export const BackgroundCircle: React.FC = () => {
   return (
-    <svg width="1181" height="1181" xmlns="http://www.w3.org/2000/svg">
-      <circle stroke="#000" stroke-width="3" cx="590.5" cy="590.5" r="588.5" fill="none" fill-rule="evenodd"/>
+    <svg width="1181" height="1181">
+      <circle stroke="#000" strokeWidth="3" cx="590.5" cy="590.5" r="588.5" fill="none" fillRule="evenodd"/>
     </svg>
   );
 };
