19083d745dbbae7044284fe27726ec9b59aa9e4a
robertschaap <robert.schaap@mac.com>
2020-11-29 11:23:25 +0100

    feat(BackgroundLine): add color

--- src/components/BackgroundLine/BackgroundLine.tsx
+++ src/components/BackgroundLine/BackgroundLine.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const BackgroundLine: React.FC = () => {
   return (
     <svg width="1067" height="1067">
-      <path stroke="#000" strokeWidth="3" d="M2.5 2.5l1062 1062" fill="none" fillRule="evenodd" strokeLinecap="square"/>
+      <path stroke="currentColor" strokeWidth="3" d="M2.5 2.5l1062 1062" fill="none" fillRule="evenodd" strokeLinecap="square"/>
     </svg>
   );
 };
