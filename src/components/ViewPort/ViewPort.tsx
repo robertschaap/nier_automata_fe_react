4302ed8eb99c93bb8f28e8a7e1bacf2ea9cbfc1b
robertschaap <robert.schaap@mac.com>
2020-11-30 21:12:51 +0100

    feat(ViewPort): add config flag for distracting elements

--- src/components/ViewPort/ViewPort.tsx
+++ src/components/ViewPort/ViewPort.tsx
@@ -4,6 +4,7 @@ import * as S from './viewport.styles';
 
 import { BackgroundCircle } from 'components/BackgroundCircle';
 import { BackgroundLine } from 'components/BackgroundLine';
+import { config } from 'utils/config';
 
 interface ViewPortProps {
   children: React.ReactNode;
@@ -13,42 +14,50 @@ export const ViewPort: React.FC<ViewPortProps> = ({ children }) => {
   return (
     <S.ViewPortBase>
       <S.Background>
-        <S.BackgroundCircle>
-          <BackgroundCircle size={1210} />
-        </S.BackgroundCircle>
-        <S.BackgroundCircle>
-          <BackgroundCircle size={1240} />
-        </S.BackgroundCircle>
-        <S.BackgroundLine offsetX={-12} offsetY={-3}>
-          <BackgroundLine />
-        </S.BackgroundLine>
-        <S.BackgroundLine>
-          <BackgroundLine />
-        </S.BackgroundLine>
-        <S.BackgroundLine offsetX={-11} offsetY={-20}>
-          <BackgroundLine />
-        </S.BackgroundLine>
-        <S.BackgroundCircle bottom>
-          <BackgroundCircle size={1210} />
-        </S.BackgroundCircle>
-        <S.BackgroundCircle bottom>
-          <BackgroundCircle size={1240} />
-        </S.BackgroundCircle>
-        <S.BackgroundLine bottom offsetX={3} offsetY={12}>
-          <BackgroundLine />
-        </S.BackgroundLine>
-        <S.BackgroundLine bottom>
-          <BackgroundLine />
-        </S.BackgroundLine>
-        <S.BackgroundLine bottom offsetX={23} offsetY={15}>
-          <BackgroundLine />
-        </S.BackgroundLine>
+        {config.SHOW_DISTRACTIONS && (
+          <>
+            <S.BackgroundCircle>
+              <BackgroundCircle size={1210} />
+            </S.BackgroundCircle>
+            <S.BackgroundCircle>
+              <BackgroundCircle size={1240} />
+            </S.BackgroundCircle>
+            <S.BackgroundLine offsetX={-12} offsetY={-3}>
+              <BackgroundLine />
+            </S.BackgroundLine>
+            <S.BackgroundLine>
+              <BackgroundLine />
+            </S.BackgroundLine>
+            <S.BackgroundLine offsetX={-11} offsetY={-20}>
+              <BackgroundLine />
+            </S.BackgroundLine>
+            <S.BackgroundCircle bottom>
+              <BackgroundCircle size={1210} />
+            </S.BackgroundCircle>
+            <S.BackgroundCircle bottom>
+              <BackgroundCircle size={1240} />
+            </S.BackgroundCircle>
+            <S.BackgroundLine bottom offsetX={3} offsetY={12}>
+              <BackgroundLine />
+            </S.BackgroundLine>
+            <S.BackgroundLine bottom>
+              <BackgroundLine />
+            </S.BackgroundLine>
+            <S.BackgroundLine bottom offsetX={23} offsetY={15}>
+              <BackgroundLine />
+            </S.BackgroundLine>
+          </>
+        )}
       </S.Background>
       <S.Layout>
         {children}
       </S.Layout>
-      <S.ForegroundRadial />
-      <S.ForegroundOverlay />
+      {config.SHOW_DISTRACTIONS && (
+        <>
+          <S.ForegroundRadial />
+          <S.ForegroundOverlay />
+        </>
+      )}
     </S.ViewPortBase>
   );
 };
