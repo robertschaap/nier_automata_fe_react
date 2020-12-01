268b30d03fb55edc6dbe0b508411060c106e21d8
robertschaap <robert.schaap@mac.com>
2020-12-01 16:32:25 +0100

    feat(WriteText): setup component

---
+++ src/components/WriteText/WriteText.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+
+import { config } from 'utils/config';
+
+const randomCharacter = (string: string) => {
+  return string[Math.floor(Math.random() * string.length)] || '';
+};
+
+const randomisedString = (string: string) => {
+  return string.split('').reduce((a, v, i) => {
+    const prefix = string.slice(0, i);
+
+    return a.concat(prefix + randomCharacter(string), prefix + randomCharacter(string), prefix + v);
+  }, [] as string[]);
+};
+
+export const WriteText: React.FC<{ text: string }> = React.memo(({ text }) => {
+  if (!config.SHOW_DISTRACTIONS) {
+    return (
+      <>{text}</>
+    );
+  }
+
+  const [writtenText, setWrittenText] = useState(randomisedString(text));
+
+  useEffect(() => {
+    let timer: number;
+
+    if (writtenText.length > 1) {
+      timer = setTimeout(() => {
+        setWrittenText(writtenText.splice(1));
+      }, 10);
+    }
+
+    return () => clearTimeout(timer);
+  }, [writtenText]);
+
+  return (
+    <>
+      {writtenText[0]}
+    </>
+  );
+});
---
+++ src/components/WriteText/index.ts
@@ -0,0 +1,1 @@
+export { WriteText } from './WriteText';
