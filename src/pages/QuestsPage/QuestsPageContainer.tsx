9dba435ae52d7b199d9d9ce4239e3aefdb858058
robertschaap <robert.schaap@mac.com>
2020-12-03 18:54:20 +0100

    feat(QuestsPageContainer): add PageHeader

--- src/pages/QuestsPage/QuestsPageContainer.tsx
+++ src/pages/QuestsPage/QuestsPageContainer.tsx
@@ -4,11 +4,13 @@ import { QuestsPage } from './QuestsPage';
 
 import { Footer } from 'components/Footer';
 import { Header } from 'components/Header';
+import { PageHeader } from 'components/PageHeader';
 
 export const QuestsPageContainer: React.FC = () => {
   return (
     <>
-      <Header title="Quests" />
+      <Header showTitle={false} title="Quests" />
+      <PageHeader title="Quests" />
       <QuestsPage />
       <Footer text="View the map or perform a quick save" />
     </>
