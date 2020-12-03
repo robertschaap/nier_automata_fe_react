c5083aacb80c636a6f2421fb182393c4e77a6183
robertschaap <robert.schaap@mac.com>
2020-12-03 18:54:11 +0100

    feat(MapPageContainer): add PageHeader

--- src/pages/MapPage/MapPageContainer.tsx
+++ src/pages/MapPage/MapPageContainer.tsx
@@ -4,13 +4,15 @@ import { MapPage } from './MapPage';
 
 import { Footer } from 'components/Footer';
 import { Header } from 'components/Header';
+import { PageHeader } from 'components/PageHeader';
 
 // TODO: the inline styles are temp, remove when there's a bit of clarity on how
 // the Main component inside(?) MapPage acts and on what Footer needs
 export const MapPageContainer: React.FC = () => {
   return (
     <>
-      <Header title="Map" />
+      <Header showTitle={false} title="Map" />
+      <PageHeader title="Map" />
       <MapPage />
       <Footer text="View the map or perform a quick-save." />
     </>
