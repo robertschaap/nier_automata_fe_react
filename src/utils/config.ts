36ef9e4631c5a68530c497a8a174d30df2c4dd66
robertschaap <robert.schaap@mac.com>
2020-12-04 19:54:27 +0100

    feat(config): export as const

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,5 +1,5 @@
 const { env } = import.meta;
 
-export const config = Object.freeze({
+export const config = {
   SHOW_DISTRACTIONS: env.SNOWPACK_PUBLIC_SHOW_DISTRACTIONS === 'true',
-});
+} as const;
